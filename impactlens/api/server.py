5b792ac2cfadb46ec819737e6d908f56886a09b7
sanstack256 <nssan2007@gmail.com>
2026-09-06 14:27:48 +0530

    Support Firebase measurementId and live env reload in server.py

--- impactlens/api/server.py
+++ impactlens/api/server.py
@@ -180,13 +180,21 @@ def integration_health():
 
 @app.get("/api/config/firebase")
 def firebase_config():
+    if not os.getenv("FIREBASE_API_KEY"):
+        try:
+            import dotenv
+            dotenv.load_dotenv(BASE_DIR / ".env", override=True)
+            dotenv.load_dotenv(BASE_DIR.parent / ".env", override=True)
+        except Exception:
+            pass
     return {
         "apiKey": os.getenv("FIREBASE_API_KEY", ""),
         "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN", ""),
         "projectId": os.getenv("FIREBASE_PROJECT_ID", ""),
         "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET", ""),
         "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID", ""),
-        "appId": os.getenv("FIREBASE_APP_ID", "")
+        "appId": os.getenv("FIREBASE_APP_ID", ""),
+        "measurementId": os.getenv("FIREBASE_MEASUREMENT_ID", "")
     }
 
 
